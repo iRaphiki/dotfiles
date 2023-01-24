367542395e1a220fc8cf75b0fc6e155a199231fa
Rafael Fernandez <imraphiki@gmail.com>
2023-01-24 08:14:56 +0100

    Adding Windows dotfiles

---
+++ Windows/.pwsh/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,74 @@
+# Funtions
+function lsHidden {
+    ls -Hidden
+}
+function st2h {
+    shutdown /s /t 7200 
+}
+function gitStatus {
+  git status
+}
+function gitAddCommit {
+  git add . && git commit
+}
+function gitCheckout {
+  git checkout
+}
+function gitLogShort {
+  git log --oneline --graph
+}
+function gitLogPretty {
+  git log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short
+}
+# Alias 
+Set-Alias winfetch pwshfetch-test-1  
+Set-Alias v nvim
+Set-Alias ll ls
+Set-Alias g goto
+Set-Alias la lsHidden 
+Set-Alias grep findstr
+Set-Alias cat bat
+Set-Alias fh filehhash
+Set-Alias gs gitStatus
+Set-Alias gac gitAddCommit 
+Set-Alias gco gitCheckout
+Set-Alias gls gitLogShort 
+Set-Alias glp gitLogPretty
+
+# Icons
+Import-Module -Name Terminal-Icons
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue | 
+  Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+function goto {
+  param (
+    $location
+  )
+
+  Switch ($location) {
+    "pj" {
+      Set-Location -Path "E:\Code\projects"
+    }
+	  "mpj" {
+		  Set-Location -Path "E:\Code\projects\iraphiki"
+	  }
+    "ws" {
+      Set-Location -Path "E:\Code\projects\iraphiki\CSA_Node\ws"
+    }
+    "cs" {
+      Set-Location -Path "E:\MyDrive\Brain"
+    } 
+    default {
+      Write-Output "Invalid location"
+    }
+  }
+}
+
+# StarShip config and var
+$ENV:STARSHIP_CONFIG = "$HOME\.config\starship.toml"
+$ENV:STARSHIP_DISTRO = "者 raphiki"
+Invoke-Expression (&starship init powershell)
